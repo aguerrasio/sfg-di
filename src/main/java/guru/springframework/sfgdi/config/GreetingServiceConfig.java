2162b6bafaa738d0801cdf24e0f90820db294841
adriana.guerrasio <adriana.guerrasio@evaluate.com>
2022-12-15 14:35:51 -0300

    initial commit

---
+++ src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
@@ -0,0 +1,8 @@
+package guru.springframework.sfgdi.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration // to define different beans
+public class GreetingServiceConfig {
+
+}
