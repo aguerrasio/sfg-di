e7fcc3ff08b262fe0a56da24ad56f4538ef2cf04
adriana.guerrasio <adriana.guerrasio@evaluate.com>
2022-12-15 15:01:37 -0300

    fixing

---
+++ src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
@@ -0,0 +1,4 @@
+package guru.springframework.sfgdi.config;
+
+public class GreetingServiceConfig {
+}
--- src/main/java/guru/springframework/sfgdi/services/CatPetService.java
+++ src/main/java/guru/springframework/sfgdi/services/CatPetService.java
@@ -1,6 +1,7 @@
 package guru.springframework.sfgdi.services;
 
 import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
 
 /**
